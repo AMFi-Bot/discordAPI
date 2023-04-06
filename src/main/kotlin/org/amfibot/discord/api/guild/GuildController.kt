5e036e8717cd5011bf8dc909d509cdbf624afb6a
Vadim Vetrov <vetrovvd@gmail.com>
2023-04-06 20:20:37 +0300

    Added PUT and DELETE methods for discord guild controller

--- src/main/kotlin/org/amfibot/discord/api/guild/GuildController.kt
+++ src/main/kotlin/org/amfibot/discord/api/guild/GuildController.kt
@@ -2,6 +2,7 @@ package org.amfibot.discord.api.guild
 
 import org.amfibot.discord.api.exceptions.crud.ResourceAlreadyExistsException
 import org.amfibot.discord.api.exceptions.crud.ResourceNotFoundException
+import org.amfibot.discord.api.exceptions.http.client.BadRequestException
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -19,14 +20,14 @@ import java.util.*
 class GuildController(@Autowired private val repository: GuildRepository) {
 
     /**
-     * Returns the discord guild object by guild id
+     * Returns a discord guild object by guild id
      */
     @GetMapping("/{guildId}")
     fun getGuild(@PathVariable guildId: String): Guild =
         repository.findById(guildId).orElseThrow { ResourceNotFoundException() }
 
     /**
-     * Creates a discord guild
+     * Creates a discord guild object
      */
     @PostMapping
     fun createGuild(@RequestBody guild: Guild) : ResponseEntity<Any?>{
@@ -38,6 +39,32 @@ class GuildController(@Autowired private val repository: GuildRepository) {
         repository.insert(guild)
 
         return ResponseEntity(HttpStatus.CREATED)
+    }
+
+
+    /**
+     * Creates a discord guild object
+     */
+    @PutMapping("/{guildId}")
+    fun updateGuild(@PathVariable guildId: String, @RequestBody guild: Guild) : ResponseEntity<Any?>{
+        if (!repository.existsById(guildId))
+            throw ResourceNotFoundException()
+
+        if (guildId != guild.id) throw BadRequestException()
+
+        repository.save(guild)
+
+        return ResponseEntity(HttpStatus.NO_CONTENT)
+    }
+
+    @DeleteMapping("/{guildId}")
+    fun deleteGuild(@PathVariable guildId: String): ResponseEntity<Any?> {
+        if (!repository.existsById(guildId))
+            throw ResourceNotFoundException()
+
+        repository.deleteById(guildId)
+
+        return ResponseEntity(HttpStatus.NO_CONTENT)
     }
 
 
