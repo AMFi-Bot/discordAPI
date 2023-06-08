134f097d4a28b39485b257aabf3d95dc538db2ca
Vadim Vetrov <vetrovvd@gmail.com>
2023-06-08 15:06:02 +0300

    Added csrf support

--- src/main/kotlin/org/amfibot/discord/api/config/SecurityConfig.kt
+++ src/main/kotlin/org/amfibot/discord/api/config/SecurityConfig.kt
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.invoke
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 class SecurityConfig {
@@ -17,11 +20,42 @@ class SecurityConfig {
             authorizeRequests {
                 authorize("/.~~spring-boot!~/**", permitAll)
                 authorize("/error", permitAll)
-                authorize("/api/discord/guilds", hasAuthority("SCOPE_discordGuilds"))
+                authorize("/api/discord/guilds", permitAll)//hasAuthority("SCOPE_discordGuilds"))
                 authorize(anyRequest, permitAll)
             }
+
+            cors {
+                configurationSource = corsConfigurationSource()
+            }
         }
-        
+
         return http.build()
+    }
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource{
+        val configuration = CorsConfiguration()
+        configuration.allowedOriginPatterns = listOf("*")
+        configuration.allowedMethods = listOf("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH")
+        configuration.allowCredentials = true
+        configuration.allowedHeaders =
+            listOf(
+                "Accept",
+                "Origin",
+                "Content-Type",
+                "Depth",
+                "User-Agent",
+                "If-Modified-Since,",
+                "Cache-Control",
+                "Authorization",
+                "X-Req",
+                "X-File-Size",
+                "X-Requested-With",
+                "X-File-Name"
+            )
+
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
     }
 }
