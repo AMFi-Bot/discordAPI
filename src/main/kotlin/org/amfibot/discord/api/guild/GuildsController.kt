dc2709457876eb1b9f70eb6c747da7e8392bc780
Vadim Vetrov <vetrovvd@gmail.com>
2023-06-11 15:36:53 +0300

    replaced get method with post because the first one does not accept the request body

--- src/main/kotlin/org/amfibot/discord/api/guild/GuildsController.kt
+++ src/main/kotlin/org/amfibot/discord/api/guild/GuildsController.kt
@@ -17,29 +17,26 @@ class GuildsController(@Autowired private val repository: GuildRepository) {
 
     /**
      * Returns only those guilds of the given list where the bot is on it.
+     *
+     * ! Uses POST because the GET request does not accept body
      */
-    @GetMapping("/registered")
-    fun isRegistered(guildsIds: Collection<String>): Collection<String>{
+    @PostMapping("/registered")
+    fun isRegistered(@RequestBody guildsIds: Collection<String>): Collection<String> {
         return repository.findAllById(guildsIds).map { guild -> guild.id }
     }
 
-    /**
-     * Checks the guild for the bot to be registered in it.
-     */
+    /** Checks the guild for the bot to be registered in it. */
     @GetMapping("/registered/{guildId}")
-    fun isRegistered(@PathVariable guildId: String): Boolean{
+    fun isRegistered(@PathVariable guildId: String): Boolean {
         return repository.findById(guildId).isPresent
     }
 
-    /**
-     * Creates a discord guild object
-     */
+    /** Creates a discord guild object */
     @PostMapping
-    fun createGuild(@RequestBody guild: Guild) : ResponseEntity<Any?> {
+    fun createGuild(@RequestBody guild: Guild): ResponseEntity<Any?> {
         val guildId = guild.id
 
-        if (repository.existsById(guildId))
-            throw ResourceAlreadyExistsException()
+        if (repository.existsById(guildId)) throw ResourceAlreadyExistsException()
 
         repository.insert(guild)
 
