4d44f4854d9dc54e07592bcd6005561310d4b588
Vadim Vetrov <vetrovvd@gmail.com>
2023-07-01 21:56:19 +0300

    fixed rabbitmq error on sending object: enforced jackson message encoder

--- src/main/kotlin/org/amfibot/discord/api/config/RabbitMQConfig.kt
+++ src/main/kotlin/org/amfibot/discord/api/config/RabbitMQConfig.kt
@@ -1,5 +1,16 @@
 package org.amfibot.discord.api.config
 
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter
+import org.springframework.amqp.support.converter.MessageConverter
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class RabbitMQConfig {
+
+    @Bean
+    fun getMessageConverter(): MessageConverter = Jackson2JsonMessageConverter()
+}
 
 enum class RabbitQueues(val queueName: String) {
     DISCORD_GUILD_REGISTERED("discord_guild.registered"),
