dbecd52317d4eb74e51a068a2395a651640631c9
Vadim Vetrov <vetrovvd@gmail.com>
2023-07-19 14:27:09 +0300

    Added guards to registered guilds listing

--- src/main/kotlin/org/amfibot/discord/api/guild/GuildsController.kt
+++ src/main/kotlin/org/amfibot/discord/api/guild/GuildsController.kt
@@ -6,10 +6,13 @@ import jakarta.servlet.http.HttpServletRequest
 import org.amfibot.discord.api.config.RabbitQueues
 import org.amfibot.discord.api.exceptions.http.client.BadRequestException
 import org.amfibot.discord.api.helpers.guild.fetchGuildOAuth2Code
+import org.amfibot.discord.api.user.User
+import org.amfibot.discord.api.user.fetchDiscordUsersGuilds
 import org.springframework.amqp.rabbit.core.RabbitTemplate
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.*
+import org.springframework.security.core.Authentication
 import org.springframework.web.bind.annotation.*
 
 /**
@@ -28,19 +31,30 @@ class GuildsController(
 
     /**
      * Returns only those guilds of the given list where the bot is on it.
+     * (And where the user is administrator)
      *
      * ! Uses POST because the GET request does not accept body
      */
     @PostMapping("/registered")
-    fun isRegistered(@RequestBody guildsIds: Collection<String>): Collection<String> {
-        return repository.findAllById(guildsIds).map { guild -> guild.id }
+    fun isRegistered(@RequestBody guildsIds: Collection<String>, authentication: Authentication): Collection<String> {
+        val user = authentication.principal as User
+
+        val userGuilds = fetchDiscordUsersGuilds(user.token)
+
+        val guilds = guildsIds.filter { guildId ->
+            userGuilds.find {
+                (it.id().asString() == guildId) && (it.permissions().get().and(0x8L) == 0x8L)
+            } != null
+        }
+
+        return repository.findAllById(guilds).map { guild -> guild.id }
     }
 
-    /** Checks the guild for the bot to be registered in it. */
+    /** Checks the guild for the bot to be registered in it. (And to user being administrator of it */
     @GetMapping("/registered/{guildId}")
-    fun isRegistered(@PathVariable guildId: String): Boolean {
-        return repository.findById(guildId).isPresent
-    }
+    fun isRegistered(@PathVariable guildId: String, authentication: Authentication): Boolean =
+        isRegistered(listOf(guildId), authentication).contains(guildId)
+
 
     /**
      * Registers a discord guild in the guilds list of the bot
